e8fa8d6beb3cd0fd24549c23cbb0c484ceb2d245
Jefry Jacky <jefryjacky@gmail.com>
2022-04-07 12:03:30 +0700

    hide refresh token

--- src/main/kotlin/com/authentication/app/controller/oauth/model/TokenResponse.kt
+++ src/main/kotlin/com/authentication/app/controller/oauth/model/TokenResponse.kt
@@ -1,8 +1,7 @@
 package com.authentication.app.controller.oauth.model
 
-import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
-import java.awt.Stroke
 
 /**
  * Created by Jefry Jacky on 13/09/20.
@@ -11,7 +10,7 @@ data class TokenResponse(
         @JsonProperty("access_token")
         val accessToken: String,
         @JsonProperty("refresh_token")
-        @JsonIgnore
+        @JsonIgnoreProperties
         val refreshToken: String,
         @JsonProperty("expired_date")
         val expiredDate: Long
